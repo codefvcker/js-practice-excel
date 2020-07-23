a51928fb8ae2bf9b72c578250c97027916dff204
aleh.siarheyeu <alehsiarheyeu@gmail.com>
2020-07-23 23:42:54 +0300

    Added layout and basic structure in excel

---
+++ src/core/DomListener.js
@@ -0,0 +1,41 @@
+import { capitalize } from './utils'
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $root provided from DomListener!')
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener)
+
+      if (!this[method]) {
+        throw new Error(
+          `Method ${method} is'nt implemented in ${this.name || ''} Component`
+        )
+      }
+
+      // Bind with context forever
+      this[method] = this[method].bind(this)
+
+      // the same as addEventListener
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener)
+
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
+}
